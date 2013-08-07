fe7accdec11df57c8fd459d23bedd5636efd9f3a
Mark Rickert <mjar81@gmail.com>
2013-08-07 10:58:35 -0400

    Add archive script

---
+++ gitarchive.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Git Archive script.
+# Takes one parameter: a remote git repository URL.
+# Creates: a gzipped tarball of all branches of the supplied URL.
+
+if [ "$1" = "" ]
+then
+echo "Usage: $0 <git repositiry clone URL>" 1>&2
+exit 1
+fi
+
+# Variable definitions
+GITURL=$1
+GITNAME=${GITURL##*/}
+FOLDERNAME=${GITNAME%.git}
+TARNAME="$FOLDERNAME.gitarchive.$(date +%Y%m%d).tgz"
+
+# Clone the repos and go into the folder
+git clone --recursive $GITURL $FOLDERNAME
+cd $FOLDERNAME
+
+# Pull all branches
+git branch -r | grep -v HEAD | grep -v master | while read branch; do
+  git branch --track ${branch##*/} $branch
+done
+
+#Pull all remote data and tags
+git fetch --all
+git fetch --tags
+git pull --all
+git gc # Cleanup unnecessary files and optimize the local repository
+
+# Create an archive of the directory
+cd ../
+tar cfz "$TARNAME" "$FOLDERNAME/"
+
+# Remove the git clone
+rm -rf "./$FOLDERNAME"
+
+echo "Done!"
+echo "Your archived git repository is named $TARNAME"
